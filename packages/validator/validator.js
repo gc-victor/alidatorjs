fd8b61cfea142a6b34eb7517c366d467036e9850
Víctor García <gc.victor@gmail.com>
2018-08-15 09:16:40 +0200

    feat(validator) add successes validation
    Get the truthy values is as needed as get the errors

--- packages/validator/validator.js
+++ packages/validator/validator.js
@@ -1,15 +1,19 @@
 export function validator(validators) {
     return {
         get every() {
-            return validators.every(condition => !!condition);
+            return validators.every(condition => condition);
         },
 
         get some() {
-            return validators.some(condition => !!condition);
+            return validators.some(condition => condition);
         },
 
         get errors() {
             return validators.filter(arr => !arr[0]).map(arr => arr[1]());
+        },
+
+        get successes() {
+            return validators.filter(arr => arr[0]).map(arr => arr[1]());
         },
     };
 }
--- packages/validator/validator.test.js
+++ packages/validator/validator.test.js
@@ -3,17 +3,11 @@ const { deepEquals, equal } = require('tap');
 const { validator } = require('./validator');
 
 equal(validator([false]).every, false);
-
 equal(validator([true]).every, true);
-
 equal(validator([true, true, true]).some, true);
-
 equal(validator([false, true, false]).some, true);
-
 equal(validator([true, true, true]).every, true);
-
 equal(validator([true, true, false]).every, false);
-
 deepEquals(
     validator([
         [true, () => {}],
@@ -23,5 +17,15 @@ deepEquals(
     ]).errors,
     ['Error 1', 'Error 2']
 );
-
 deepEquals(validator([[true, () => {}], [true, () => {}]]).errors, []);
+deepEquals(
+    validator([
+        [true, () => 'Success 1'],
+        [false, () => {}],
+        [true, () => 'Success 2'],
+        [false, () => {}],
+    ]).successes,
+    ['Success 1', 'Success 2']
+);
+deepEquals(validator([[false, () => {}]]).successes, []);
+deepEquals(validator([[true, () => {}]]).successes, [null]);
